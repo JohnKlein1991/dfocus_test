a97db94e567686f11f42b5a6ae113554434ddc09
andrew_marhai <buk2018irinam@gmail.com>
2019-08-31 05:48:38 +0300

    test

--- task2/intefaces/ITest.php
+++ task2/intefaces/ITest.php
@@ -8,6 +8,7 @@ interface ITest
 {
     public function __construct($data);
 
+    //возвращает массив вопросов с ответами пользователя
     public function getAnswers();
 
     //в бд в таблице algo_to_tests находим какие алгоритмы нужны для данного теста и возвращаем их названия в виде
